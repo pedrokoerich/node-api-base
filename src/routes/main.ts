829ebff5fa5c5372285b26f310d4792f1ac0e82b
Pedro H. Koerich <pedro.koerich@totvs.com.br>
2025-05-28 13:56:14 -0300

    integrando o prisma ao projeto

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import { prisma } from '../libs/prisma';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.get('/teste', (req, res) => {
+})
