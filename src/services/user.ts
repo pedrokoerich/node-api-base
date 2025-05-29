ab8d5e339218c4a727ed5c001f0d1e2d5a266d7f
Pedro H. Koerich <pedro.koerich@totvs.com.br>
2025-05-29 10:31:33 -0300

    upserts

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,23 +2,35 @@ import { Prisma } from "../generated/prisma";
 import { prisma } from "../libs/prisma"
 
 export const createUser= async (data: Prisma.UserCreateInput) => {
-    try {
-        const user = await prisma.user.create({
-            data
-        })
+    //try {
+    //    const user = await prisma.user.create({
+    //        data
+    //    })
 
-        return user;
+    //    return user;
 
-    } catch (error) {
-        if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            if (error.code === 'P2002') {
-                console.error('Email already exists:', data.email);
-            } else {
-                console.error('Prisma error:', error.message);
-            }
-        }
-        return false
-    }
+    //} catch (error) {
+    //    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    //        if (error.code === 'P2002') {
+    //            console.error('Email already exists:', data.email);
+    //        } else {
+    //            console.error('Prisma error:', error.message);
+    //        }
+    //    }
+    //    return false
+    //}
+
+    const user = await prisma.user.upsert({
+        where : {
+            email: data.email
+        }, 
+        update: {
+            role: 'ADMIN'
+        }, 
+        create: data
+    })
+
+    return user;
 }
 
 export const createUsers = async (users: Prisma.UserCreateInput[]) => {
